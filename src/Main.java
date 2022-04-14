2b54c074c43b4836b96b8e3868ebb015629ca11f
Natalia Zubareva <n.zubareva@inbox.ru>
2022-04-14 13:12:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameProgress progress1 = new GameProgress(500,4,5,234);
+        GameProgress progress2 = new GameProgress(85500,76,85,4);
+        GameProgress progress3 = new GameProgress(7500,34,25,23);
+
+        saveGame("D:\\Games(netology)\\savegames\\save1.dat", progress1);
+        saveGame("D:\\Games(netology)\\savegames\\save2.dat", progress2);
+        saveGame("D:\\Games(netology)\\savegames\\save3.dat", progress3);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("D:\\Games(netology)\\savegames\\save1.dat");
+        list.add("D:\\Games(netology)\\savegames\\save2.dat");
+        list.add("D:\\Games(netology)\\savegames\\save3.dat");
+
+        zipFiles("D:\\Games(netology)\\savegames\\save.zip", list);
+        deleteUnpackingFiles();
+
+    }
+
+    public static void saveGame(String path, GameProgress progress) {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(progress);
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void zipFiles(String path, List<String> list) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
+            int count = 1;
+            int i;
+            for (String s : list) {
+                try (FileInputStream fis = new FileInputStream(s)) {
+                    ZipEntry entry = new ZipEntry("packed_save" + count++ + ".dat");
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void deleteUnpackingFiles() {
+        File files = new File("D:\\Games(netology)\\savegames");
+        if(files.isDirectory()) {
+            for (File f : files.listFiles()) {
+                if (!f.getName().contains(".zip")) {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+}
